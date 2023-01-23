fb3a2cf5b745734058b0969121bc6f1b7d9ee044
sarveshrane2000 <sarveshrane2000@gmail.com>
2023-01-23 22:40:10 +0530

    removed: @astrojs/cloudflare

--- astro.config.mjs
+++ astro.config.mjs
@@ -10,14 +10,10 @@ import sitemap from "@astrojs/sitemap";
 import prefetch from "@astrojs/prefetch";
 
 // https://astro.build/config
-import cloudflare from "@astrojs/cloudflare";
-
-// https://astro.build/config
 import image from "@astrojs/image";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), sitemap(), prefetch(), image()],
-  output: "server",
-  adapter: cloudflare()
+	site: "https://sarveshrane.pages.dev",
+	integrations: [tailwind(), sitemap(), prefetch(), image()],
 });

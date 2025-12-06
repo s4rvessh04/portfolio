520a0e42c47fe3349bd12ccdae3799202e6c68cd
s4rvessh04 <sarveshrane2000@gmail.com>
2025-12-06 13:18:24 +0530

    add: new profile and skills updated

--- src/data.ts
+++ src/data.ts
@@ -1,89 +1,102 @@
 export const githubUsername = "s4rvessh04";
 
 export const dataCurrently = [
-	{ title: "Engineering Student" },
-	{ title: "Full Stack Developer" },
-	{ title: "UI/UX Designer" },
+  { title: "Software Engineer" },
+  { title: "Backend Developer" },
+  { title: "DevOps Enthusiast" },
 ];
 
 export const socialLinks = [
-	{ title: "GitHub", link: `https://github.com/${githubUsername}` },
-	{
-		title: "LinkedIn",
-		link: "https://linkedin.com/in/sarvesh-rane-80452522a",
-	},
-	{ title: "Instagram", link: "https://www.instagram.com/sarve.ssh_/" },
-	{ title: "Twitter/X", link: "https://twitter.com/sarvessh_/" },
-	{ title: "Gmail", link: "mailto:sarveshrane2000.portfolio-site@gmail.com" },
+  { title: "GitHub", link: `https://github.com/${githubUsername}` },
+  {
+    title: "LinkedIn",
+    link: "https://linkedin.com/in/sarvesh-rane-80452522a",
+  },
+  { title: "Instagram", link: "https://www.instagram.com/sarve.ssh_/" },
+  { title: "Twitter/X", link: "https://twitter.com/sarvessh_/" },
+  { title: "Gmail", link: "mailto:sarveshrane2000.portfolio-site@gmail.com" },
 ];
 
 export const projects = [
-	{
-		title: "Budgeter",
-		link: `https://github.com/${githubUsername}/Budgeter_v2`,
-		description:
-			"A simple, personal, financial wellness utility based on React and Django",
-	},
-	{
-		title: "Vartalap",
-		link: `https://github.com/${githubUsername}/vartalap-nextjs`,
-		description: "A realtime chat-app built using NextJS",
-	},
-	{
-		title: "Blog Site",
-		link: `https://github.com/${githubUsername}/astro-blog-site`,
-		description: "Personal blogging website built with Astro 🚀",
-	},
-	{
-		title: "University Portal",
-		link: `https://github.com/${githubUsername}/University-Website`,
-		description: "A university portal built with Django",
-	},
-	{
-		title: "DSA",
-		link: `https://github.com/${githubUsername}/DSA`,
-		description: "My learnings of DSA, curated in Python, Java and Go",
-	},
+  {
+    title: "Budgeter",
+    link: `https://github.com/${githubUsername}/Budgeter_v2`,
+    description:
+      "A simple, personal, financial wellness utility based on React and Django",
+  },
+  {
+    title: "Vartalap",
+    link: `https://github.com/${githubUsername}/vartalap-nextjs`,
+    description: "A realtime chat-app built using NextJS",
+  },
+  {
+    title: "Blog Site",
+    link: `https://github.com/${githubUsername}/astro-blog-site`,
+    description: "Personal blogging website built with Astro 🚀",
+  },
+  {
+    title: "University Portal",
+    link: `https://github.com/${githubUsername}/University-Website`,
+    description: "A university portal built with Django",
+  },
+  {
+    title: "DSA",
+    link: `https://github.com/${githubUsername}/DSA`,
+    description: "My learnings of DSA, curated in Python, Java and Go",
+  },
 ];
 
 export const skills = [
-	{
-		title: "Programming Languages",
-		data: [
-			{ title: "python", level: "Advanced" },
-			{ title: "JavaScript", level: "Intermediate" },
-			{ title: "Typescript", level: "Intermediate" },
-			{ title: "Java", level: "Medium" },
-			{ title: "Go", level: "Medium" },
-		],
-	},
-	{
-		title: "Frameworks",
-		data: [
-			{ title: "React", level: "Advanced" },
-			{ title: "Django", level: "Advanced" },
-			{ title: "Astro", level: "Intermediate" },
-			{ title: "Svelte", level: "Intermediate" },
-			{ title: "FastAPI", level: "Intermediate" },
-			{ title: "NextJS", level: "Beginner" },
-		],
-	},
-	{
-		title: "Databases",
-		data: [
-			{ title: "MySQL", level: "Intermediate" },
-			{ title: "SQLite", level: "Intermediate" },
-			{ title: "MongoDB", level: "Beginner" },
-		],
-	},
-	{
-		title: "Technologies",
-		data: [
-			{ title: "Git", level: "Intermediate" },
-			{ title: "HTML", level: "Advanced" },
-			{ title: "CSS", level: "Advanced" },
-			{ title: "Figma", level: "Advanced" },
-			{ title: "Linux", level: "Intermediate" },
-		],
-	},
+  {
+    title: "Programming Languages",
+    data: [
+      { title: "JavaScript", level: "Advanced" },
+      { title: "Python", level: "Advanced" },
+      { title: "Go", level: "Medium" },
+      { title: "Java", level: "Medium" },
+    ],
+  },
+  {
+    title: "Frameworks",
+    data: [
+      { title: "ExpressJS", level: "Advanced" },
+      { title: "HonoJS", level: "Advanced" },
+      { title: "React", level: "Advanced" },
+      { title: "Django", level: "Advanced" },
+      { title: "FastAPI", level: "Intermediate" },
+      { title: "NextJS", level: "Beginner" },
+    ],
+  },
+  {
+    title: "Databases",
+    data: [
+      { title: "MySQL", level: "Intermediate" },
+      { title: "Postgresql", level: "Intermediate" },
+      { title: "SQLite", level: "Intermediate" },
+      { title: "MongoDB", level: "Beginner" },
+    ],
+  },
+  {
+    title: "Technologies",
+    data: [
+      { title: "Git", level: "Intermediate" },
+      { title: "Docker", level: "Advanced" },
+      { title: "ELK stack", level: "Advanced" },
+      { title: "K8s", level: "Advanced" },
+      { title: "Redis", level: "Advanced" },
+      { title: "Apache Kafka", level: "Begineer" },
+      { title: "Linux", level: "Intermediate" },
+    ],
+  },
 ];
+
+export const professionalSummary = `
+Software Engineer with 2+ years of hands-on experience building and scaling backend systems for real-world, enterprise-level products. 
+I’ve worked across a small fleet of microservices, boosted overall throughput by 40%, 
+and helped cut critical production bugs by 50% after launch. 
+I love building clean, scalable services and 
+I actively use Generative AI to speed up development, improve code quality, and debug smarter.
+Always looking to push performance, ship faster, and level up the tech wherever I go.
+Apart from coding, I enjoy traveling, attending events, and listening to
+EDM music to stay inspired and motivated.
+`
